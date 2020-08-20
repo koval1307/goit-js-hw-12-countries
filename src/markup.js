26534c78d5dd601a0fbaaadfbb07896bd775a440
Koval <dmytrokovalskyi@yahoo.com>
2020-08-20 12:38:24 +0300

    Update markup.js

--- src/markup.js
+++ src/markup.js
@@ -9,20 +9,20 @@ import {noCountries} from './notification'
 function markupCountries() {
     const query = refs.input.value;
     if (query) {
-        return fetchCountries(query).then(data => {
-            if (data.length > 10) {
-                errorMessage();
-                refs.countries.innerHTML = '';
-            } else if (data.length <= 10) {
-                refs.countries.innerHTML = countriesListTpl(data);
-            } else if (data.length === 1) {
-                     refs.countries.innerHTML = countryTpl(data);
-                   } else {
-                     noCountries();
-                     refs.countries.innerHTML = "";
-                   }
-        });
-    } 
+return fetchCountries(query).then(data => {
+if (data.length > 10) {
+ errorMessage();
+ refs.countries.innerHTML = '';
+   } else if (data.length <= 10) {
+   refs.countries.innerHTML = countriesListTpl(data);
+ } else if (data.length === 1) {
+refs.countries.innerHTML = countryTpl(data);
+} else {
+ noCountries();
+  refs.countries.innerHTML = "";
+ }
+});
+ } 
 }
 
 
